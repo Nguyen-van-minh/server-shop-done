9b88d522262b25d07ad5895d0fc8216a107bc5a4
Minh <2001minhnguyen@gmail.com>
2022-07-03 23:31:06 +0700

    up

--- index.js
+++ index.js
@@ -9,14 +9,9 @@ const cartRouter = require('./route/cartRoute')
 const information = require('./route/informationRoute')
 const review = require('./route/reviewRoute')
 const vnpay = require('./route/vnpayRoute')
-const mg = require('mailgun-js');
 const mongoose = require('mongoose')
 const cors = require('cors')
 require('dotenv').config()
-
-
-
-
 
 const app = express();
 app.use(express.json())
@@ -53,37 +48,7 @@ app.use('/api/information', information)
 app.use('/api/review', review)
 app.use('/api/vnpay', vnpay)
 
-const mailgun = () =>
-    mg({
-        apiKey: "87b64c51e8bde708a076e6c45cd728c5-77985560-68872229",
-        domain: "sandbox40086c5ab4a2464fb48f38313ad5a9f2.mailgun.org",
-    });
 
-app.post('/api/email', (req, res) => {
-    const { email, subject, message } = req.body;
-    console.log(req.body)
-    mailgun()
-        .messages()
-        .send(
-            {
-                from: "Beautiful <beautiful@mg.yourdomain.com>",
-                to: `${email}`,
-                subject: `${subject}`,
-                text: `${message}`,
-            },
-            (error, body) => {
-                if (error) {
-                    console.log(error);
-                    res.status(500).send({ message: 'Error in sending email' });
-                } else {
-                    console.log(body);
-                    res.send({ message: 'Email sent successfully' });
-                }
-            }
-        );
-});
+const PORT = process.env.PORT || 5000
 
-
-// const PORT = process.env.PORT || 5000
-const PORT = 5000
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`))
